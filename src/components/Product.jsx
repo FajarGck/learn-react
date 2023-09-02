5d2d9347a5b6ee7bc63e3f7eb67341da36a94225
fajar436 <111217865+FajarGck@users.noreply.github.com>
2023-09-02 16:47:04 +0700

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ import './style/style.css'
 function Product({ name, price, location}) {
   return (
     <div className='card-product'>
-        <img src="https://placeholder.com/200" alt="dummy" />
+        <img src="https://placeholder.com" alt="dummy" />
         <h4>{name}</h4>
         <h3>${price}</h3>
         <h5>{location}</h5>
